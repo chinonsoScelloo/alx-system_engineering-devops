8c07cadec1c1cedf9223da733e64031d355bd498
Chinonso Onaga <76971567+chinonsoScelloo@users.noreply.github.com>
2023-04-26 00:04:35 +0100

    update

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,10 +1,4 @@
-package { 'python3-pip':
-  ensure => 'installed',
-}
-
 exec { 'install_flask':
   command => '/usr/bin/pip3 install Flask==2.1.0',
-  path    => ['/usr/bin', '/usr/local/bin'],
-  unless  => '/usr/bin/pip3 show Flask | grep Version | grep -q 2.1.0',
   require => Package['python3-pip'],
 }
